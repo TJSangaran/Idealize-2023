2da2ff6a975348f380a272d9c073ffde630ac1da
TJSangaran <thevarasasangaran@gmail.com>
2023-09-02 23:57:40 +0530

    changes in side nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard.jsx";
+import About from "./pages/About.jsx";
+import Analytics from "./pages/Analytics.jsx";
+import Comment from "./pages/Comment.jsx";
+import Product from "./pages/Product.jsx";
+import ProductList from "./pages/ProductList.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import home from "../assets/icons/home.svg";
+import ambulance from "../assets/icons/ambulance.svg";
+import drivers from "../assets/icons/drivers.svg";
+import help from "../assets/icons/help.svg";
+import logout from "../assets/icons/logout.svg";
+
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/home",
+      name: "Home",
+      icon: <img src={home} alt="" />,
+    },
+    {
+      path: "/am",
+      name: "Aambulance",
+      icon: <img src={ambulance} alt="" />,
+    },
+    {
+      path: "/drivers",
+      name: "Drivers",
+      icon: <img src={drivers} alt="" />,
+    },
+    {
+      path: "/help",
+      name: "Help",
+      icon: <img src={help} alt="" />,
+    },
+    {
+      path: "/logout",
+      name: "Logout",
+      icon: <img src={logout} alt="" />,
+    },
+  ];
+  return (
+    <div className="container">
+      <div
+        style={{ width: isOpen ? "283.179px" : "87.418px", height: "973px" }}
+        className="sidebar"
+      >
+        <div className="top_section">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            LifeSaver
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
